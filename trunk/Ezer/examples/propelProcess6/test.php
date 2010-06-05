dbb537fd85dea342e474fd448d3ec9b72891fa4e
johnathan.kanarek <johnathan.kanarek@424eaab0-b641-11de-8dfb-43c0dffa5306>
2010-06-05 16:00:33 +0000

    VERSION 1.1.2 Support DB persistence for process instances

--- trunk/Ezer/examples/propelProcess6/test.php
+++ trunk/Ezer/examples/propelProcess6/test.php
@@ -6,7 +6,7 @@ require_once 'bootstrap.php';
 $config = Ezer_Config::createFromPath('config.xml');
 $dbConfig = $config->database->toArray();
 
-$server = new Ezer_BusinessProcessServer(new Ezer_PropelLogicPersistance($dbConfig), new Ezer_PropelInstancePersistance($dbConfig));
+$server = new Ezer_BusinessProcessServer(new Ezer_PropelLogicPersistance($dbConfig));
 $server->addCasePersistance(new Ezer_PropelCasePersistance($dbConfig));
 
 $server->addThreadClient(new Ezer_BusinessProcessClient($server, $config->phpExe));
