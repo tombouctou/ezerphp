8e0194a5ea690952feff1e6e144a706959355aa5
johnathan.kanarek <johnathan.kanarek@424eaab0-b641-11de-8dfb-43c0dffa5306>
2010-06-05 10:43:11 +0000

    VERSION 1.1.1 Support XML persistance of process instance

--- Ezer/examples/syncProcess1/test.php
+++ Ezer/examples/syncProcess1/test.php
@@ -3,9 +3,9 @@ ini_set('max_execution_time', 0);
 
 require_once 'bootstrap.php';
 
-$config = new Ezer_Config('config.xml');
+$config = Ezer_Config::createFromPath('config.xml');
 
-$server = new Ezer_BusinessProcessServer(new Ezer_XmlLogicPersistance($config->logicPath));
+$server = new Ezer_BusinessProcessServer(new Ezer_XmlLogicPersistance($config->logicPath, $config->instancePath));
 
 $server->addCasePersistance(new Ezer_XmlCasePersistance($config->casesPath));
 
