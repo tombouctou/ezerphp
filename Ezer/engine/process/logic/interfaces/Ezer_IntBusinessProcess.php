d9c0d6418125125e012fdc30a18c76c8ab202eee
johnathan.kanarek <johnathan.kanarek@424eaab0-b641-11de-8dfb-43c0dffa5306>
2010-06-05 10:54:10 +0000

    VERSION 1.1.0 Support autoloading and DB persistence over Propel

--- Ezer/engine/process/logic/interfaces/Ezer_IntBusinessProcess.php
+++ Ezer/engine/process/logic/interfaces/Ezer_IntBusinessProcess.php
@@ -5,4 +5,10 @@ interface Ezer_IntBusinessProcess extends Ezer_IntScope
 	 * @return array<Ezer_IntImport>
 	 */
 	public function getImports();
+	
+	/**
+	 * @param array $variables
+	 * @return Ezer_BusinessProcessInstance
+	 */
+	public function &createBusinessProcessInstance(array $variables);
 }
