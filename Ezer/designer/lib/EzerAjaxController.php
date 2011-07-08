2adec74ebe7286eece2a9d296e19e74e92f6ec88
johnathan.kanarek <johnathan.kanarek@424eaab0-b641-11de-8dfb-43c0dffa5306>
2011-07-08 15:50:02 +0000

    Added object translate methods

--- Ezer/designer/lib/EzerAjaxController.php
+++ Ezer/designer/lib/EzerAjaxController.php
@@ -1,5 +1,42 @@
 <?php
 class EzerAjaxController
 {
-	
+	/**
+	 * @param array $objects
+	 * @param array $columns
+	 * @return array<stdClass>
+	 */
+	protected function toArray(array $objects, array $columns)
+	{
+		$ret = array();
+		foreach($objects as $object)
+			$ret[] = $this->toObject($object, $columns);
+			
+		return $ret;
+	}
+
+	/**
+	 * @param BaseObject $objects
+	 * @param array $columns
+	 * @return stdClass
+	 */
+	protected function toObject(BaseObject $object, array $columns)
+	{
+		$matches = null;
+		if(!preg_match('/^Ezer_Propel(.+)$/', get_class($object), $matches))
+			return null;
+			
+		$stdClass = new stdClass();
+		$stdClass->objectType = $matches[1];
+		
+		foreach($columns as $column)
+		{
+			if($column == 'data')
+				continue;
+				
+			$getter = "get{$column}";
+			$stdClass->$column = $object->$getter();
+		}
+		return $stdClass;
+	}
 }
