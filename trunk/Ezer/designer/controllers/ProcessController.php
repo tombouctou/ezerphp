dac48d8df95212a9234261ae70893ee24a07841f
johnathan.kanarek <johnathan.kanarek@424eaab0-b641-11de-8dfb-43c0dffa5306>
2011-07-08 15:51:02 +0000

    Use translate methods

--- trunk/Ezer/designer/controllers/ProcessController.php
+++ trunk/Ezer/designer/controllers/ProcessController.php
@@ -3,22 +3,9 @@ class ProcessController extends EzerAjaxController
 {
 	public function listAction()
 	{
-		$processes = Ezer_PropelStepPeer::retrieveActiveProcesses();
+		$objects = Ezer_PropelStepPeer::retrieveActiveProcesses();
 		$columns = Ezer_PropelStepPeer::getFieldNames(BasePeer::TYPE_STUDLYPHPNAME);
 		
-		$ret = array();
-		foreach($processes as $process)
-		{
-			$processObject = new stdClass();
-			foreach($columns as $column)
-			{
-				$getter = "get{$column}";
-				$processObject->$column = $process->$getter();
-			}
-			
-			$ret[] = $processObject;
-		}
-			
-		return $ret;
+		return $this->toArray($objects, $columns);
 	}
 }
