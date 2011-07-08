045389bc895e33e1d7fa33320dfdc120ae6b2db1
johnathan.kanarek <johnathan.kanarek@424eaab0-b641-11de-8dfb-43c0dffa5306>
2011-07-08 15:50:26 +0000

    Added request translate to methods parameters

--- Ezer/designer/lib/EzerAjaxFrontController.php
+++ Ezer/designer/lib/EzerAjaxFrontController.php
@@ -23,7 +23,73 @@ class EzerAjaxFrontController
 		if(!method_exists($controller, $actionMethod))
 			die("Action [{$controllerName}.{$actionName}] not found");
 			
-		$results = $controller->$actionMethod();
+		$arguments = array();
+		try
+		{
+			$method = new ReflectionMethod($controllerClass, $actionMethod);
+		}
+		catch(Exception $e)
+		{
+			echo "Action not found [$controllerName.$actionName]";
+			return;
+		}
+		
+		$parameters = $method->getParameters();
+		foreach($parameters as $parameter)
+		{
+			$arguments[$parameter->name] = self::getArgument($parameter);
+		}
+		
+		$results = $method->invokeArgs($controller, $arguments);
 		echo json_encode($results);
+	}
+	
+	private static function getArgumentType(ReflectionParameter $parameter)
+	{
+		$comment = $parameter->getDeclaringFunction()->getDocComment();
+		if(!$comment)
+			return null;
+			
+		$matches = null;
+		if(!preg_match("/@param\\s+([^\\s]+)\\s+\\\${$parameter->name}/", $comment, $matches))
+			return null;
+		
+		return $matches[1];
+	}
+	
+	private static function getArgument(ReflectionParameter $parameter)
+	{
+		if(isset($_REQUEST[$parameter->name]))
+		{
+			if($parameter->isArray())
+				return $_REQUEST[$parameter->name];
+				
+			$type = self::getArgumentType($parameter);
+			if(!class_exists($type))
+				throw new Exception("Type not found for attribute [$parameter->name]");
+				
+			$object = new $type();
+			if(is_array($_REQUEST[$parameter->name]))
+			{
+				foreach($_REQUEST[$parameter->name] as $attribute => $value)
+					$object->$attribute = $value;
+			}
+			return $object;
+		}
+		
+		if($parameter->isDefaultValueAvailable())
+			return $parameter->getDefaultValue();
+			
+		if($parameter->allowsNull())
+			return null;
+					
+		if($parameter->isArray())
+			return array();
+			
+		$type = self::getArgumentType($parameter);
+		if(!class_exists($type))
+			throw new Exception("Type not found for attribute [$parameter->name]");
+			
+		return new $type();
 	}
 }
