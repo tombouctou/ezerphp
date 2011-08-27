155f45a60ed852a0658fd659ec1dc8601940e749
johnathan.kanarek <johnathan.kanarek@gmail.com>
2011-08-27 09:22:35 +0000

    changed my mail

--- Ezer/engine/core/threads/Ezer_ThreadClient.php
+++ Ezer/engine/core/threads/Ezer_ThreadClient.php
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * For questions, help, comments, discussion, etc., please send
- * e-mail to tan-tan@simple.co.il
+ * e-mail to johnathan.kanarek@gmail.com
  */
 
 
